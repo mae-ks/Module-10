e1be95803b49550cbc91310f9ec7bad7df775110
Mae Kyaw San <mae.kyawsan@bison.howard.edu>
2021-04-04 00:59:00 -0400

    Test

--- code.py
+++ code.py
@@ -1,22 +1,22 @@
 import sys
-import fileinput
 
-def asci(shift, file):
+def asci(shift, sentence):
     modif, res = "", ""
     tracker = 0
-    with open(file, 'r') as file:
-      for sentence in file:
-          for letter in sentence:
-              if letter.isalpha():
-                  modif += letter.upper()
+    for letter in sentence:
+        if letter.isalpha():
+            modif += letter.upper()
     for tracker in range(len(modif)):
         new_ord = ord(modif[tracker]) + int(shift)
         if new_ord > 90:
             new_ord = new_ord - 26
         new_chr = chr(new_ord)
-        if tracker%5 == 0 and tracker != 0:
+        if tracker%50==0 and tracker !=0:
+            res += "\n"
+        elif tracker%5 == 0 and tracker != 0:
             res += " "
         res += new_chr
     return res
 
-print(asci(sys.argv[1], sys.argv[2]))
+sent = input()
+print(asci(sys.argv[1], sent))
