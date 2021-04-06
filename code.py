cdf6e70feb6cd529e750d6466a7ebe3a2637bf72
Mae Kyaw San <mae.kyawsan@bison.howard.edu>
2021-04-06 03:50:34 +0000

    Final2

--- code.py
+++ code.py
@@ -9,15 +9,15 @@ def asci(shift):
         if letter.isalpha():
           modif += letter.upper()
     for tracker in range(len(modif)):
-        new_ord = ord(modif[tracker]) + int(shift)
-        if new_ord > 90:
-            new_ord = new_ord - 26
-        new_chr = chr(new_ord)
-        if tracker%50==0 and tracker !=0:
-            res += "\n"
-        elif tracker%5 == 0 and tracker != 0:
-            res += " "
-        res += new_chr
+      new_ord = ord(modif[tracker]) + int(shift)
+      if new_ord > 90:
+          new_ord = new_ord - 26
+      new_chr = chr(new_ord)
+      if tracker%50==0 and tracker !=0:
+          res += "\n"
+      elif tracker%5 == 0 and tracker != 0:
+          res += " "
+      res += new_chr
     return res
 
 print(asci(sys.argv[1]))
